89c3edf5c0360d523fe90824e78a1e1f2a5c4760
SergioVelaAlfajarin <fdezvela.sergio.2002@gmail.com>
2022-05-02 14:20:54 +0200

    Terminada version 0.9. Añadidos loggers

--- java/src/gestor/Logger.java
+++ java/src/gestor/Logger.java
@@ -1,38 +1,58 @@
 package gestor;
 
 import exception.LogException;
+import exception.XMLException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
 public abstract class Logger {
 	private static File logFile = null;
 	private static PrintWriter escritor = null;
 
-
-
-	private static boolean valido;
-
-	public static void init(String pathToLog){
-		compruebaPath(pathToLog);
-		if(!valido){
-			try {
-				logFile = new File(pathToLog);
-				escritor = new PrintWriter(logFile);
-				valido = true;
-			} catch (FileNotFoundException e) {
-				valido = false;
-			}
+	public static void init(String pathToLog) throws LogException {
+		try{
+			compruebaPath(pathToLog);
+			logFile = new File(pathToLog);
+			escritor = new PrintWriter(logFile);
+			Logger.print("Comienza la ejecucion");
+			Logger.print("Se ha inicializado el logger. Ruta: " + logFile.getAbsolutePath());
+		}catch(LogException | FileNotFoundException e){
+			throw new LogException("Ha habido un problema con el logger.");
 		}
 	}
 
 	private static void compruebaPath(String p) throws LogException {
 		if(p == null || p.length() == 0)
 			throw new LogException("La ruta del logger no es valida.");
+
+		if(logFile != null){
+			File tmp = new File(p);
+			String pathInicial = logFile.getAbsolutePath();
+			String pathTmp = tmp.getAbsolutePath();
+
+			if(pathInicial.equalsIgnoreCase(pathTmp)){
+				throw new LogException("La ruta del logger es la misma que la anterior.");
+			}
+		}
 	}
 
-	public static void printLog(String log){
+	public static void print(String log) throws LogException{
+		if(escritor == null){
+			throw new LogException("El logger no esta inicializado");
+		}
+		String hora = LocalDateTime.now().toString();
+		String cadena = String.format("[%s] -> %s%n", hora, log);
+		escritor.print(cadena);
+		escritor.flush();
+	}
 
+	public static void close() throws LogException{
+		if(escritor == null){
+			throw new LogException("El logger no esta inicializado");
+		}
+		escritor.close();
 	}
 }
--- java/src/main/Main.java
+++ java/src/main/Main.java
@@ -1,8 +1,10 @@
 package main;
 
+import exception.LogException;
 import exception.XMLException;
 import gestor.EscritorXML;
 import gestor.LectorXML;
+import gestor.Logger;
 import modelo.AparcamientoBicicleta;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -14,7 +16,7 @@ public class Main {
 	private static final String RUTA_XML = "ficheros/aparcamiento-bicicleta.xml";
 	private static final String RUTA_XML_NUEVA = "ficheros/aparcamiento.xml";
 	private static final String RUTA_XML_INVALIDOS = "ficheros/llevar_bicis.dat";
-
+	private static final String RUTA_LOG = "ficheros/log.txt";
 	/**
 	 * Metodo main
 	 * <hr/>
@@ -27,17 +29,30 @@ public class Main {
 		EscritorXML escritor = new EscritorXML();
 
 		try{
+			//unica linea que puede lanzar LogException
+			Logger.init(RUTA_LOG);
 			lector.leerArchivoXML(RUTA_XML);
+
+			//lee nodos y los agrega a la coleccion
 			NodeList nodes = lector.getNodosPrincipales();
 			ArrayList<Element> listaElements = nodeListToArray(nodes);
+			Logger.print("Lista de nodos transformada correctamente a array.");
+			Logger.print("Iterando y transformando en Objeto...");
 			for(Element el: listaElements){
 				AparcamientoBicicleta apb = lector.transformElementToAparcamiento(el);
 				escritor.addAparcamientoBicicleta(apb);
 			}
+			Logger.print("Array de elements Transformado correctamente.");
+
+			//escribe los aparcamientos de la coleccion
 			escritor.escribeAparcamientosEnArchivo(RUTA_XML_NUEVA,RUTA_XML_INVALIDOS);
+			Logger.print("Aparcamientos Escritos. Ejecucion terminada.");
 		}catch(XMLException e){
-			//escribir en el logger el error y imprimir
-			// "Ha ocurrido algun error. Por favor, comprueba el archivo ficheros/logger.txt".
+			Logger.print(e.getMessage());
+			Logger.close();
+			System.out.println("Ha ocurrido un problema. Por favor compruebe el archivo " +
+					"'ficheros/log.txt'");
+		}catch(LogException e){
 			System.out.println(e.getMessage());
 		}
 	}
